c972bd1e5907c189a006e0b59ecd171f1b42c551
paul61302 <paul61302@gmail.com>
2020-07-31 16:32:59 +0800

    123

---
+++ AR_Class/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator ani;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            ani.SetBool("IsRun", true);
+        }
+        else
+        {
+            ani.SetBool("IsRun", false);
+        }
+    }
+}
